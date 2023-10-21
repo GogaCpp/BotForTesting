a399f5bafa8949fa1263d6d306d20c7b190b0439
beautiful_far_away <assdddffff54@mail.ru>
2023-10-21 21:59:58 +0300

    Initial commit

---
+++ src/main/java/com/bot/botfortesting/BotForTestingApplication.java
@@ -0,0 +1,13 @@
+package com.bot.botfortesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotForTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotForTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/botfortesting/BotForTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.botfortesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotForTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
