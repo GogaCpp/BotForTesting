3f866a3120d0dc833113c34c5b2c22ec07cc1fcd
jenya-second <jenya.simf2004@gmail.com>
2023-10-25 20:06:48 +0300

    Add project files

---
+++ src/main/java/jenya/gogacpypy/GogacpypyApplication.java
@@ -0,0 +1,13 @@
+package jenya.gogacpypy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogacpypyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogacpypyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jenya/gogacpypy/GogacpypyApplicationTests.java
@@ -0,0 +1,13 @@
+package jenya.gogacpypy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogacpypyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
