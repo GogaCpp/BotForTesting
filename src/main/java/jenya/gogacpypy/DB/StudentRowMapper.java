4468686507b0f242ebdf3cea877dbb1b2f62e6c1
jenya-second <jenya.simf2004@gmail.com>
2023-11-08 19:41:11 +0300

    Page with add students data, page with view students data

---
+++ src/main/java/jenya/gogacpypy/DB/StudentRowMapper.java
@@ -0,0 +1,17 @@
+package jenya.gogacpypy.DB;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class StudentRowMapper implements RowMapper<Student> {
+    @Override
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Student student = new Student();
+        student.setId(rs.getLong("id"));
+        student.setName(rs.getString("name"));
+        student.setEmail(rs.getString("email"));
+        return student;
+
+    }
+}
--- src/main/java/jenya/gogacpypy/GogacpypyApplication.java
+++ src/main/java/jenya/gogacpypy/GogacpypyApplication.java
@@ -1,13 +1,50 @@
 package jenya.gogacpypy;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jenya.gogacpypy.DB.Student;
+import jenya.gogacpypy.DB.StudentRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@SpringBootApplication
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class GogacpypyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GogacpypyApplication.class, args);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/form")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("Student", new Student());
+        return "form";
+    }
+
+    @GetMapping("/view")
+    public String view(Model model) {
+        String sql = "SELECT * from test";
+        List<Student> data = jdbcTemplate.query(sql,new StudentRowMapper());
+        model.addAttribute("students", data);
+        return "view";
+    }
+    @PostMapping ("/form_submit")
+    public String form_submit(@ModelAttribute("Student") Student student,
+                              BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        String sql = "INSERT INTO test (name, email) VALUES ('"
+                +student.getName() + "','" + student.getEmail() + "')";
+
+        int rows = jdbcTemplate.update(sql);
+        return "form";
     }
 
 }
